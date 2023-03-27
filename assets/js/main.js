245290d665f95fa6bbb1968b850c434bb8b8d724
JureD <jure.dvorsak@outlook.com>
2023-03-27 12:04:21 +0200

    Started working on mobile version.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,10 @@ function hamburgerMenu() {
     for (let i = 0; i < menu_entries.length; i++) {
         if (menu_entries[i].style.display === "block") {
             menu_entries[i].style.display = "none";
+            document.getElementsByClassName("gradient")[0].style.top = "11.3rem";
         } else {
-            menu_entries[i].style.display = "block";;
+            menu_entries[i].style.display = "block";
+            document.getElementsByClassName("gradient")[0].style.top = "26.3rem";
         }
     }
   }
